97d24118e7fb46faa4d785eda9c7d5f15f69abdf
name <email>
2017-10-17 23:21:11 +0530

    Rename variables and make minor changes

--- hits.py
+++ hits.py
@@ -2,41 +2,39 @@ import numpy
 import scipy
 
 class HITS():
-    def __init__(self,matrix,trmatrix):
-        self.__matrix = matrix
-        self.__trmatrix = trmatrix
-        self.__n= self.__matrix.shape[0]
-        self.__hubs= numpy.ones((self.__n,1))
-        self.__auth= numpy.ones((self.__n,1))
+    def __init__(self, link_matrix):
+        self.__link_matrix = matrix
+        self.__link_matrix_tr = link_matrix.transpose()
+        self.__n = self.__link_matrix.shape[0]
+        self.__hubs = numpy.ones((self.__n,1))
+        self.__auths = numpy.ones((self.__n,1))
 
-    def calcRank(self):
+    def calc_scores(self):
         epsilon = 0.001
-        iter=0
+        epsilon_matrix = epsilon * (numpy.ones((self.__n,1)).all())
+
         while True:
-            iter+=1
-
             hubs_old= self.__hubs
-            self.__auth= numpy.matmul(self.__trmatrix, hubs_old)
-            self.__auth= self.__auth/self.__auth.max(axis=0)
 
-            self.__hubs= numpy.matmul(self.__matrix, self.__auth)
-            self.__hubs= self.__hubs/self.__hubs.max(axis=0)
+            self.__auths = numpy.matmul(self.__link_matrix_tr, hubs_old)
+            self.__auths = self.__auths / self.__auths.max(axis=0)
 
-            if ((abs(self.__hubs - hubs_old))< epsilon*numpy.ones((self.__n,1))).all():
+            self.__hubs = numpy.matmul(self.__link_matrix, self.__auths)
+            self.__hubs = self.__hubs / self.__hubs.max(axis=0)
+
+            if ((abs(self.__hubs - hubs_old))< epsilon_matrix).all():
                 break
-        #print(iter)
 
-    def getHubs(self):
+    def get_hubs(self):
         return self.__hubs
 
-    def getAuth(self):
-        return self.__auth
+    def get_auths(self):
+        return self.__auths
 
 def main():
     a=[[0,1,1,1,0],[1,0,0,1,0],[0,0,0,0,1],[0,1,1,0,0],[0,0,0,0,0]]
     b= numpy.array(a)
-    c= b.transpose()
-    p = HITS(b,c)
+    p = HITS(b)
     p.calcRank()
     print(p.getHubs())
     print(p.getAuth())
