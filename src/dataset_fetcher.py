8d4147250047c81e40fcb1fc50fa95d4f9c701d6
name <email>
2017-11-04 15:02:03 +0530

    Changes

--- src/dataset_fetcher.py
+++ src/dataset_fetcher.py
@@ -5,26 +5,60 @@ import pickle
 import numpy as np
 import scipy.sparse as sparse
 
-debug = True
+debug = False
+log = True
 
 class DatasetFetcher():
-	def __init__(self, key, secret):
+	def __init__(self, key, secret, log_path):
 		auth = tweepy.AppAuthHandler(key, secret)
 		self._api = tweepy.API(auth)
 		self._visited = None
 		self._graph = None
 
 	def _print_api_rem(self):
-		temp = self._api.rate_limit_status()
-		print('friends endpoint remaining: ', temp['resources']['friends']['/friends/list']['remaining'])
-		print('followers endpoint remaining: ', temp['resources']['followers']['/followers/list']['remaining'])
+		try:
+			temp = self._api.rate_limit_status()
+		except tweepy.RateLimitError:
+			print('Print API limit reached')
+		except Exception as e:
+			print('Print API limit exception: ', e)
+		else:
+			print('Friends endpoint remaining: ', temp['resources']['friends']['/friends/list']['remaining'])
+			print('Followers endpoint remaining: ', temp['resources']['followers']['/followers/list']['remaining'])
 
-	def get_dataset(self, seed_user, limit, limit_on='explored'):
+	def _handle_limit(self, cursor, friends_or_followers):
+		while True:
+			try:
+				yield cursor.next()
+			except tweepy.RateLimitError:
+				try:
+					reset_time = self._api.rate_limit_status()['resources'][friends_or_followers]['/' + friends_or_followers + '/list']['reset']
+				except tweepy.RateLimitError:
+					print('Sleeping for ', 15 * 60, ' seconds')
+					time.sleep(15 * 60)
+				except Exception as e:
+					print('Unexpected exception thrown: ', e)
+					print('Sleeping for ', 15 * 60, ' seconds')
+					time.sleep(15 * 60)
+				else:
+					print('Sleeping for ', max(reset_time - time.time() + 1, 1), ' seconds')
+					time.sleep(max(reset_time - time.time() + 1, 1))
+
+
+	def get_dataset(self, seed_user, friends_limit, followers_limit, limit_on, limit, live_save, users_path, adj_list_path):
 		"""\
 			seed_user is the id/screen_name/name
 			of the user to start the bfs with
+
+			friends_limit is the number of friends to consider for each user
+
+			followers_limit is the number of followers to consider for each user
+
 			limit_on can be either explored or visited
+
+			limit is the limit corresponding to limit_on
 		"""
+
 		self._print_api_rem()
 		# three possible states -
 		# unvisited, visited but not explored, explored
@@ -59,54 +93,57 @@ class DatasetFetcher():
 
 		# get the graph
 		limited_var_val = 0
+		live_save_suffix = 0
 		while limited_var_val < limit and not boundary.empty():
+			print()
+			self._print_api_rem()
 			user_id = boundary.get()
-			if debug:
-				print('\nselected: ', self._visited[user_id]['screen_name'])
-
-			rate_limit_info = self._api.rate_limit_status()['resources']['friends']['/friends/list']
-			while rate_limit_info['remaining'] == 0:
-				self._print_api_rem()
-				time.sleep(max(rate_limit_info['reset'] - (int)(time.time()), 1))
-			
-			friends = self._api.friends(user_id)
-			if debug:
-				print('friends: ')
-			for i in friends:
-				if i.id not in self._visited:
-					self._visited[i.id] = {
-						'name': i.name,
-						'screen_name': i.screen_name
+			if debug or log:
+				print('Selected:', self._visited[user_id]['screen_name'])
+  
+			# friends
+			if log:
+				print('Finding friends..')
+			cnt = 0
+			for friend in self._handle_limit(tweepy.Cursor(self._api.friends, user_id=user_id).items(friends_limit), 'friends'):
+				cnt += 1
+				if friend.id not in self._visited:
+					self._visited[friend.id] = {
+						'name': friend.name,
+						'screen_name': friend.screen_name
 					}
-					self._graph[i.id] = {
+					self._graph[friend.id] = {
 						'friends': [],
 						'followers': []
 					}
-					boundary.put(i.id)
+					boundary.put(friend.id)
+				self._graph[user_id]['friends'].append(friend.id)
 				if debug:
-					print(self._visited[i.id]['screen_name'], end=' ')
-				self._graph[user_id]['friends'].append(i.id)
+					print(self._visited[friend.id]['screen_name'], end=' ')
+			if log:
+				print('Found', cnt, 'friends')
 
-			rate_limit_info = self._api.rate_limit_status()['resources']['followers']['/followers/list']
-			while rate_limit_info['remaining'] == 0:
-				time.sleep(max(rate_limit_info['reset'] - (int)(time.time()), 1))
-			followers = self._api.followers(user_id)
-			if debug:
-				print('\nfollowers: ')
-			for i in followers:
-				if i.id not in self._visited:
-					self._visited[i.id] = {
-						'name': i.name,
-						'screen_name': i.screen_name
+			# followers
+			if log:
+				print('Finding followers..')
+			cnt = 0
+			for follower in self._handle_limit(tweepy.Cursor(self._api.followers, user_id=user_id).items(followers_limit), 'followers'):
+				cnt += 1
+				if follower.id not in self._visited:
+					self._visited[follower.id] = {
+						'name': follower.name,
+						'screen_name': follower.screen_name
 					}
-					self._graph[i.id] = {
+					self._graph[follower.id] = {
 						'friends': [],
 						'followers': []
 					}
-					boundary.put(i.id)
+					boundary.put(follower.id)
+				self._graph[user_id]['followers'].append(follower.id)
 				if debug:
-					print(self._visited[i.id]['screen_name'], end=' ')
-				self._graph[user_id]['followers'].append(i.id)
+					print(self._visited[follower.id]['screen_name'], end=' ')
+			if log:
+				print('Found', cnt, 'followers')
 
 			if limit_on == 'explored':
 				limited_var_val = len(self._visited) - boundary.qsize()
@@ -114,6 +151,12 @@ class DatasetFetcher():
 				limited_var_val = len(self._visited)
 			if debug:
 				print('\nlen(visited): ', len(self._visited), 'qsize: ', boundary.qsize())
+
+			if log:
+				print('Latest save suffix: ', live_save_suffix % 2)
+			if live_save:
+				self.save_dataset(users_path + str(live_save_suffix % 2), adj_list_path + str(live_save_suffix % 2))
+			live_save_suffix += 1
 
 		if debug:
 			for i in self._graph:
@@ -186,15 +229,28 @@ def main():
 		np.set_printoptions(threshold=np.inf)
 	key = 'j5idDIRvUfwI1213Nr14Drh33'
 	secret = 'jOw1Dgt8dJlu4rPh3GeoGofnIV5VKLkZ8fOQqYk1zUsaSMJnVl'
-	seed_user = 'n1khl'
+	seed_user = 'genius1238'
+
+	log_path = 'logs.txt'
 
 	users_path = '../data/users'
 	adj_list_path = '../data/adj_list'
 	map_path = '../data/map'
 	link_matrix_path = '../data/link_matrix'
+	
+	users_temp_path = '../data/temp/users_'
+	adj_list_temp_path = '../data/temp/adj_list_'
 
-	app = DatasetFetcher(key, secret)
-	app.get_dataset(seed_user, 3, limit_on='explored')
+	friends_limit = 200
+	followers_limit = 200
+	limit = 40
+
+	app = DatasetFetcher(key, secret, log_path)
+	if log:
+		print('Obtaining dataset..')
+	app.get_dataset(seed_user, friends_limit, followers_limit, 'explored', limit, True, users_temp_path, adj_list_temp_path)
+	if log:
+		print('Dataset obtained!')
 	app.save_dataset(users_path, adj_list_path)
 
 	# adjacency list created by dataset fetcher is used to generate the link matrix
